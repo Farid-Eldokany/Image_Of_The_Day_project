2e3600060b28cd5f27069902dd884bcade4b1563
Callum McLuskey <2469682M@student.gla.ac.uk>
2021-03-16 20:49:07 +0000

    Fixed the formatting of the homepage - callum

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -3,6 +3,7 @@ $(function(){
   // .click works on every browser besides safari change this //
 
   $(".search").click(function(){
+    $(this).removeClass("remove-input")
     $(this).addClass("search-active");
   });
 
@@ -10,6 +11,7 @@ $(function(){
     var $target = $(event.target);
     if(!$target.closest(".search").length && $(".search").hasClass("search-active")){
       $(".search").removeClass("search-active");
+      $(".search").addClass("remove-input")
     }
   });
 });
---
+++ staticfiles/js/home.js
@@ -0,0 +1,28 @@
+$(function(){
+	$(".loved-it-text").hide();
+	$(".loved-it-counter").hide();
+	$(".hated-it-text").hide();
+	$(".hated-it-counter").hide();
+
+	$(".loved-it").mouseover(function(){
+		$(".loved-it-text").show();
+		$(".loved-it-counter").show();
+	});
+
+	$(".loved-it").mouseout(function(){
+		$(".loved-it-text").hide();
+		$(".loved-it-counter").hide();
+	})
+
+	
+
+	$(".hated-it").mouseover(function(){
+		$(".hated-it-text").show();
+		$(".hated-it-counter").show();
+	});
+
+	$(".hated-it").mouseout(function(){
+		$(".hated-it-text").hide();
+		$(".hated-it-counter").hide();
+	})
+})
