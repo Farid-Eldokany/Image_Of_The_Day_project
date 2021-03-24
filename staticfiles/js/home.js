7d2504ff6df3680e809a46b4531f4e0c6c72de19
Callum McLuskey <2469682M@student.gla.ac.uk>
2021-03-24 01:53:54 +0000

    Addition of upload style change along side minor style changes across the board

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -7,20 +7,28 @@ $(function(){
 	$(".loved-it").mouseover(function(){
 		$(".loved-it-text").show();
 		$(".loved-it-counter").show();
+		$(".hated-it-text").show();
+		$(".hated-it-counter").show();
 	});
 
 	$(".loved-it").mouseout(function(){
 		$(".loved-it-text").hide();
 		$(".loved-it-counter").hide();
+		$(".hated-it-text").hide();
+		$(".hated-it-counter").hide();
 	})
 
 	$(".hated-it").mouseover(function(){
 		$(".hated-it-text").show();
 		$(".hated-it-counter").show();
+		$(".loved-it-text").show();
+		$(".loved-it-counter").show();
 	});
 
 	$(".hated-it").mouseout(function(){
 		$(".hated-it-text").hide();
 		$(".hated-it-counter").hide();
+		$(".loved-it-text").hide();
+		$(".loved-it-counter").hide();
 	})
 })
