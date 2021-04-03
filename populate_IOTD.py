e91fddac409c9391a097f87d20d6a937f2b71f99
Pharoah-beep <77456641+Pharoah-beep@users.noreply.github.com>
2021-04-03 21:17:54 +0200

    Add files via upload

--- populate_IOTD.py
+++ populate_IOTD.py
@@ -23,7 +23,7 @@ def add_userProfile(user_id,picture,likes,dislikes,name,image_id):
             'username': 'user'+str(user_id),
             'password': 'user'+str(user_id)}
     user=User.objects.create_user(**credentials)
-    total=Total.object.get_or_create(user=user,likes=0,dislikes=0)
+    total=Total.objects.get_or_create(user=user,likes=0,dislikes=0)
     total.save()
     p = UserProfile.objects.get_or_create(user=user,picture=picture,likes=likes,dislikes=dislikes,name=name,image_id=image_id)[0]
     p.save()
